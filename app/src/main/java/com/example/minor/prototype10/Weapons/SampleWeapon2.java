f2708272e4f833fa8e4f76ab1d1dfb3c13d8400e
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-04-15 01:23:35 +0900

    Reamoved an dirty comment.

--- app/src/main/java/com/example/minor/prototype10/Weapons/SampleWeapon2.java
+++ app/src/main/java/com/example/minor/prototype10/Weapons/SampleWeapon2.java
@@ -1,4 +1,18 @@
 package com.example.minor.prototype10.Weapons;
 
 public class SampleWeapon2 {
+    private int atk = 2;
+
+    public void skill1(){
+
+    }
+    public void skill2(){
+
+    }
+    public void skill3(){
+
+    }
+    public void skill4(){
+
+    }
 }
